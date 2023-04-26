bb5c3db27883a70ed4e88d410afd981a288aa1e5
Xander van Veen <493379@student.fontys.nl>
2023-04-26 13:33:49 +0200

    Uploaded all current code

---
+++ index.js
@@ -0,0 +1,62 @@
+// Allows use of admin ui on: https://admin.socket.io
+const { instrument } = require('@socket.io/admin-ui');
+
+// Setup of all requirements
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+// Setup of cors
+app.use(cors());
+
+// Create the server
+const server = http.createServer(app);
+
+// Configure socket.io
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+// When a user connects, handle all further events
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    // Disconnecting a user from the server
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+
+    // Removing user from a room
+    socket.on("leave_room", (data, cb) => {
+        socket.leave(data.room);
+        console.log(`User with ID: ${socket.id} left room: ${data.room}`);
+        cb(`Left ${data.room}`);
+    });
+
+    // Adding user to a room
+    socket.on("join_room", (data, cb) => {
+        socket.join(data.room);
+        console.log(`User with ID: ${socket.id} joined room: ${data.room}`);
+        cb(`Joined ${data.room}`);
+    });
+
+    // Handling the send message event
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('receive_message', data);
+        // Add feature that lets the sender know they sent a message
+        // cb is not working, search for something else or hope it suddenly does
+    });
+});
+
+// Start the server
+server.listen(4500, () => {
+    console.log('listening on *:4500');
+});
+
+// Extra code for admin ui
+instrument(io, { auth: false });
